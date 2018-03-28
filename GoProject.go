96c87596c84c5aa9ecaa0c070ebfc5e11e003f6f
davidclarke1995 <davidclarke1995is@gmail.com>
2018-03-29 00:07:51 +0100

    Second commit, shunt

--- GoProject.go
+++ GoProject.go
@@ -16,9 +16,45 @@ func intopost(infix string) string {
 
 	//Stack which stores operators from the infix regular expression
 	s := []rune{}
+	
+	//Loop over input
+	for _, r := range infix {
+		switch {
+
+		case r == '(':
+		//has open bracket at the end of the stack
+			s = append(s, r)
+		case r == ')':
+			for s[len(s)-1] != '(' {
+				pofix = append(pofix, s[len(s)-1]) //last element of 's'
+				s = s[:len(s)-1]                   //everything in 's' except for the last character
+			}
+
+			s = s[:len(s)-1]
+		//if a special character is found
+		case specials[r] > 0:
+			//for while the stack still has an element on it and the precedence of the current character that reads is <= the precedence of the top element of the stack
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				//Takes stack character and puts into pofix
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+
+			s = append(s, r)
+		//Not a bracket or a special character
+		default:
+			//Takes the default characters and puts it at the end of pofix
+			pofix = append(pofix, r)
+		}
+	}
+
+	//append to pofix whatever is left on the stack
+	for len(s) > 0 {
+		pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+	}
 
 	return string(pofix)
-}
+
+} 
 
 func main() {
 	//Answer: ab.c*.
